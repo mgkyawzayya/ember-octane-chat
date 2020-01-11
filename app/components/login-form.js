7f8ec0c4ab7b0e2f03965f7ffc38fb7d7e6e1941
KyawZayya <kyawzayya.ucsp@gmail.com>
2020-01-12 01:42:08 +0630

    Step 10 Complete

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,18 +1,21 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from "@ember/service";
+import AuthService from 'shlack/services/auth';
 
 export default class LoginFormComponent extends Component {
   @tracked
   userId = null;
+  /**
+   * @type {AuthService}
+   */ 
+  @service auth
   
   get isDisabled() {
     return !this.userId;
   }
-  
-  loginAsUserWithId(val) {
-    console.log('UserId: ', val);
-  }
+
   /**
    * 
    * @param {Event & {target: HTMLselectElement}} evt 
@@ -30,6 +33,6 @@ export default class LoginFormComponent extends Component {
     const {target} = evt;
     const val = target.querySelector('select').value;
     evt.preventDefault();
-    this.loginAsUserWithId(val);
+    this.auth.loginWithUserId(val);
   }
 }
