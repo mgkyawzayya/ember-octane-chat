6225bef2a66e68b575510df7b7cd9e65ee118d24
KyawZayya <kyawzayya.ucsp@gmail.com>
2020-01-12 17:37:09 +0630

    Step 12 Complete

---
+++ app/components/team-sidebar.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { inject as service } from "@ember/service";
+import AuthService from 'shlack/services/auth';
+
+export default class TeamSidebarComponent extends Component {
+  /** 
+   * @type {AuthService}
+   */
+  @service auth;
+
+  
+
+}
--- tests/stubs/auth-service.js
+++ tests/stubs/auth-service.js
@@ -1,27 +1,24 @@
-import Service from '@ember/service';
+import Service from "@ember/service";
 import { inject as service } from '@ember/service';
 import Router from '@ember/routing/router';
 import { action } from "@ember/object";
 
-const AUTH_KEY = 'shlack-userid';
-
-export default class AuthService extends Service {
+export default class MockAuthService extends Service {
+  currentUserId = null;
   /**
    * @type {Router}
    */
   @service router;
 
-  get currentUserId() {
-    return window.localStorage.getItem(AUTH_KEY);
-  }
-
   loginWithUserId(userId) {
-    window.localStorage.setItem(AUTH_KEY, userId);
+    debugger;
+    this.currentUserId = userId;
     this.router.transitionTo('teams');
   }
+
   @action
   logout() {
-    window.localStorage.removeItem(AUTH_KEY);
+    this.currentUserId = null;
     this.router.transitionTo('login');
   }
 }
